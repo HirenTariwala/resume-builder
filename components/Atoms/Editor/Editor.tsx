32f70f0cd407b808f6e2cd316eff540b46263d44
hirentariwala <hirentariwala19@gmail.com>
2024-02-06 13:45:25 +0530

    remove extra config from ck editor

--- components/Atoms/Editor/Editor.tsx
+++ components/Atoms/Editor/Editor.tsx
@@ -15,42 +15,7 @@ const Editor = (editorProps: IEditorProps) => {
     <CKEditor
       editor={ClassicEditor}
       config={{
-        toolbar: [
-          'heading',
-          '|',
-          'bold',
-          'italic',
-          'link',
-          'bulletedList',
-          '|',
-          'insertTable',
-          'undo',
-          'redo',
-        ],
-        heading: {
-          options: [
-            {
-              model: 'paragraph',
-              title: 'Paragraph',
-              class: 'ck-heading_paragraph',
-            },
-            {
-              model: 'heading1',
-              view: 'h1',
-              title: 'Heading 1',
-              class: 'ck-heading_heading1',
-            },
-            {
-              model: 'heading2',
-              view: 'h2',
-              title: 'Heading 2',
-              class: 'ck-heading_heading2',
-            },
-          ],
-        },
-        table: {
-          contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
-        },
+        toolbar: ['bold', 'italic', '|', 'bulletedList', '|', 'undo', 'redo'],
       }}
       data={data}
       onChange={(_, editor) => {
